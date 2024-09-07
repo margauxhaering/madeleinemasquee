a534e69f34c348e1fc9bc36d8256414418013edf
Margaux Haering <margauxhaering@orange.fr>
2024-09-07 17:40:11 +0200

    finally final upd

--- assets/js/global.js
+++
@@ -1,8 +0,0 @@
-var clicked = false;
-var genes = [];
-var genesList = [];
-var highLighCol = "rgb(0, 199, 166)";
-var analysis = document.querySelector('input[name = "analysis"]:checked').value;
-var sel = document.getElementById('integrativeAnalysis');
-var gene = "";
-var dataset = "";
